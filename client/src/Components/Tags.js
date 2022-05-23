19e8b678364f3f368c7b11a89d9eb214a31c4d91
Marie Nsimba <mariensimba@outlook.be>
2022-05-23 20:01:08 +0200

    Added styling to Tags Component

--- client/src/Components/Tags.js
+++ client/src/Components/Tags.js
@@ -9,20 +9,35 @@ const global = new Global()
 export default function Tags({projectId, data}) {
 
     var i = 0;
+    const style = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'start',
+        marginBottom: 10,
+    }
+
+    const etiquetteStyle = {
+        fontSize: '12px',
+        padding: '5px 10px',
+        margin: '0 5px'
+    }
 
     return (
-        data.map(tag => {
-            i++
-            var color = global.randomColor(palette, color => { 
-                return !color.includes('Salmon') && !color.includes('WhiteSmoke')
-            })
-           return <Etiquette key={`${projectId}_tagButton${i}`} variant='text'
-                value={tag}
-                fontStyle='bold'
-                color = { color.color } 
-                bgcolor = { color.addAlpha(0.3)}
-            />
-        })
+        <div style={style}>
+            {data.map(tag => {
+                i++
+                var color = global.randomColor(palette, color => { 
+                    return !color.includes('Salmon') && !color.includes('WhiteSmoke')
+                })
+                return <Etiquette key={`${projectId}_tagButton${i}`} variant='text'
+                    value={tag}
+                    fontStyle='bold'
+                    color = { color.color } 
+                    bgcolor = { color.addAlpha(0.3)}
+                    sx = {etiquetteStyle}
+                />
+            })}
+        </div>
     )
 }
 
